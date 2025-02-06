8be6e29e96c0f061fb2e3d39b27cfdbd7287e018
b.dastan <dastan.beksultanov@optimabank.kg>
2025-02-06 10:56:46 +0600

    initial

---
+++ app/src/main/java/kg/optima/mobile/doc/printersample/MainActivity.kt
@@ -0,0 +1,37 @@
+package kg.optima.mobile.doc.printersample
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+	
+	private val button by lazy {
+		findViewById<Button>(R.id.button)
+	}
+	
+	private val textView by lazy {
+		findViewById<TextView>(R.id.button)
+	}
+	
+	private val editText by lazy {
+		findViewById<EditText>(R.id.button)
+	}
+	
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		enableEdgeToEdge()
+		setContentView(R.layout.activity_main)
+		ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+			val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+			v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+			insets
+		}
+	}
+	
+}
